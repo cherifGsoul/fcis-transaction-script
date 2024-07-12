5bc9b1ffdb188dbf415005296b7e977ab3485988
Cherif Bouchelaghem <cherif.bouchelaghem@gmail.com>
2024-07-12 19:18:43 -0400

    identity signature

--- src/identityaccess/identity/index.ts
+++ src/identityaccess/identity/index.ts
@@ -6,11 +6,20 @@ export type SignInCommand = Record<string, string>
 export type UserData = Record<string, string>
 
 
-export interface IndetityService {
+export interface IdentityService {
   signUp(command: SignUpCommand): Promise<UserData>;
 
   SignIn(command: SignInCommand): Promise<void>;
 }
-class Identity {
+class DefaultIdentityService implements IdentityService {
+
+  async signUp(command: SignUpCommand): Promise<UserData> {
+    throw new Error("Method not implemented.");
+  }
+  
+  async SignIn(command: SignInCommand): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  
   
 }
